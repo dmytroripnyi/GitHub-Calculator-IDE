45c7419d4afe6b996a8135717c8456d40b1cddd7
dmytroripnyi <ripnyidima11@gmail.com>
2023-01-29 21:31:21 +0200

    Add division

--- src/app/Main.java
+++ src/app/Main.java
@@ -13,6 +13,8 @@ public class Main {
         System.out.println(d);
         int s = subtraction(a, b);
         System.out.println(s);
+        int v = division(a, b);
+        System.out.println(v);
     }
 
     private static int add(int a, int b) {
@@ -26,4 +28,7 @@ public class Main {
         return a - b;
     }
 
+    private static int division(int a, int b) {
+        return a / b;
+    }
 }
